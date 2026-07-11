e4a7cf2ad14935440f0e0a1b0e3207e094c32410
Kimmimaru <kseniasereda12.01@gmail.com>
2026-07-11 13:30:52 +0300

    feat: feedback widget — components, types, styles, modern-normalize

---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import css from './Notification.module.css'
+
+function Notification(): JSX.Element {
+  return <p className={css.message}>No feedback yet</p>
+}
+
+export default Notification
